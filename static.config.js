66f498a10f271ea24782398a3c0b1ead6b0c51a2
Matthew Bridges <mattbridgesbusiness@gmail.com>
2019-03-05 12:01:47 +0000

    update site root

--- static.config.js
+++ static.config.js
@@ -3,7 +3,7 @@ export default {
     title: "M A T T'S - W O R L D",
   }),
   getRoutes: async () => [],
-  siteRoot: 'https://assets.matt-static.s3-website-eu-west-1.amazonaws.com',
+  siteRoot: 'https://indigotextureboy.com/',
   paths: {
     dist: 'dist',
     devDist: 'dist-rstatic',
